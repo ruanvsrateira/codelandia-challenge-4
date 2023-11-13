4efeb2a0d6c78ddbbb6385f5d0ca1edc600c7f90
ruanvsrateira <vruan85@gmail.com>
2023-11-12 22:28:49 -0300

    config: setting global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyles } from "./config/globalStyles.ts";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
