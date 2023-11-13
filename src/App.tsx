8b7058b17a277ecf51f4fb08375699f4444fae22
ruanvsrateira <vruan85@gmail.com>
2023-11-12 22:32:20 -0300

    feat: create left part to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { TitleStyled } from "./styles";
+import { Fragment } from "react";
+import { Main, SectionLeft, SectionRight } from "./styles";
+import Banner from "./assets/images/banner.svg";
 
 function App() {
-  return <TitleStyled>Hello world</TitleStyled>;
+  return (
+    <Fragment>
+      <Main>
+        <SectionLeft>
+          <img src={Banner} alt="Image with any user icons" />
+        </SectionLeft>
+        <SectionRight></SectionRight>
+      </Main>
+    </Fragment>
+  );
 }
 
 export default App;
--- src/styles.ts
+++ src/styles.ts
@@ -3,3 +3,24 @@ import styled from "styled-components";
 export const TitleStyled = styled.h1`
   color: red;
 `;
+
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SectionLeft = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 100vh;
+  background-color: #f7fafc;
+`;
+
+export const SectionRight = styled.section`
+  display: flex;
+  width: 50%;
+  height: 100vh;
+`;
